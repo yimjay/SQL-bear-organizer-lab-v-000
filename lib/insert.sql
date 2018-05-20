b7b787dbbd3f6fe9691c0839f535707d5c8fba86
Ji Yim <gsooyim@gmail.com>
2018-05-20 20:47:46 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL);
