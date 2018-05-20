fd09d83129ef78d29bc84c95dbe815a3face9014
Ji Yim <gsooyim@gmail.com>
2018-05-20 20:56:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES ("Mr. Chocolate");
-INSERT INTO bears ("Rowdy", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears ("Tabitha", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears ("Sergeant Brown", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears ("Melissa", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears ("Grinch", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears ("Wendy", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears (NULL);
+INSERT INTO bears VALUES (1, "Mr. Chocolate");
+INSERT INTO bears VALUES (2, "Rowdy", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (5, "Melissa", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (6, "Grinch", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (7, "Wendy", 1, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (8, NULL);
