82c29bdc6bf1b11636f35b8d6940f5634560c266
Ji Yim <gsooyim@gmail.com>
2018-05-20 20:58:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "F", "Brown", "Angry", 1);
 INSERT INTO bears VALUES (5, "Melissa", 1, "F", "Brown", "Angry", 1);
 INSERT INTO bears VALUES (6, "Grinch", 1, "F", "Brown", "Angry", 1);
 INSERT INTO bears VALUES (7, "Wendy", 1, "F", "Brown", "Angry", 1);
-INSERT INTO bears VALUES (8, NULL);
+INSERT INTO bears VALUES (8, NULL, 1, "F", "Brown", "Angry", 1);
